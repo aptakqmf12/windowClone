a697adab1045cd0b9ce7c7e6c4fbcd9d763db1d2
김태완 <twkin0708@riskzero.kr>
2023-05-04 15:59:42 +0900

    end point fix

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://dev-api.riskzero.ai",
+        target: "http://localhost",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
         secure: false,
