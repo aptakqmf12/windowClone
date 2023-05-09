bd35642f512d141fd61074027920f250dc1ed106
ykchj1011 <unecom1231>
2023-05-09 18:48:59 +0900

    패키지센터 / 퇴직자목록 ui 작업

--- src/components/station/packageCenter/index.tsx
+++ src/components/station/packageCenter/index.tsx
@@ -1,55 +1,266 @@
-import { Box, Grid, Typography } from "@mui/material";
-import { Apps } from "@mui/icons-material";
-
 import { useState } from "react";
+import styled from "styled-components";
+import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
+import {
+  Divider,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@mui/material";
+import { Apps, ArrowForward, Home, People } from "@mui/icons-material";
+import NestedAccordion from "@components/layout/nestedAccordion";
+import RetireesManagement from "./sub/retireesList";
+import RetireesList from "./sub/retireesList";
 
-enum TabMenu {
-  TODO,
-  MEMO,
-  WORKER,
+const enum PathType {
+  DASHBOARD = "대시보드 관리",
+  CONSTRUCTION_OVERVIEW = "공사개요",
+  WORKER_MANAGE = "근로자 관리",
+  WORKER_LIST = "근로자 목록",
+  WORKER_ENTERANCE_MANAGE = "근로자 출역관리",
+  RETIREES_LIST = "퇴직자 목록",
+
+  WORKER_SAFETY_CHECK = "근로자 안전점검",
+  OCCUPATIONAL_SAFETY_AND_HEALTH_ACT = "안전보건지지침",
+  LEGAL_INFORMATION = "법령정보",
+  SCHEDULER = "스케쥴러",
+  MEMO = "MEMO",
+  TO_DO_LIST_MANAGE = "TO DO LIST 관리",
+  EMERGENCY_CALL = "비상연락망",
+  RISK_NOTIFICATION = "위험알림",
 }
 
 export default function PackageCenter() {
-  const [tab, setTab] = useState<TabMenu | undefined>();
+  const [currentPath, setCurrentPath] = useState<PathType | undefined>();
 
-  const FIXED_TABS = [
+  const renderCompontntByPath = (path: PathType) => {
+    switch (path) {
+      case PathType.DASHBOARD:
+        return <div>대시보드</div>;
+      case PathType.CONSTRUCTION_OVERVIEW:
+        return <div>공사개요</div>;
+      case PathType.WORKER_MANAGE:
+        return <div>근로자 관리</div>;
+      case PathType.WORKER_LIST:
+        return <div>근로자 목록</div>;
+      case PathType.WORKER_ENTERANCE_MANAGE:
+        return <div>근로자 출역관리</div>;
+      case PathType.RETIREES_LIST:
+        return <RetireesList />;
+      case PathType.WORKER_SAFETY_CHECK:
+        return <div>근로자 안전점검</div>;
+      case PathType.OCCUPATIONAL_SAFETY_AND_HEALTH_ACT:
+        return <div>안전보건지지침</div>;
+      case PathType.LEGAL_INFORMATION:
+        return <div>법령정보</div>;
+      case PathType.SCHEDULER:
+        return <div>스케쥴러</div>;
+      case PathType.MEMO:
+        return <div>MEMO</div>;
+      case PathType.TO_DO_LIST_MANAGE:
+        return <div>TO DO LIST 관리</div>;
+      case PathType.EMERGENCY_CALL:
+        return <div>비상연락망</div>;
+      case PathType.RISK_NOTIFICATION:
+        return <div>위험알림</div>;
+
+      default:
+        return <></>;
+    }
+  };
+  const FIXED_LIST_DATA = [
     {
-      name: "todo",
-      type: TabMenu.TODO,
+      name: "Home",
+      icon: <Home />,
+      path: undefined,
+      onClick: () => {
+        setCurrentPath(undefined);
+      },
+    },
+    {
+      name: "대시보드 관리",
+      icon: <ArrowForward />,
+      path: PathType.DASHBOARD,
+      onClick: () => {
+        setCurrentPath(PathType.DASHBOARD);
+      },
+    },
+    {
+      name: "공사개요",
+      icon: <ArrowForward />,
+      path: PathType.CONSTRUCTION_OVERVIEW,
+      onClick: () => {
+        setCurrentPath(PathType.CONSTRUCTION_OVERVIEW);
+      },
     },
 
+    {
+      name: "근로자 관리",
+      icon: <ArrowForward />,
+      path: PathType.WORKER_MANAGE,
+      onClick: () => {},
+      childList: [
+        {
+          name: "근로자 목록",
+          icon: <People />,
+          path: PathType.WORKER_MANAGE,
+          onClick: () => {
+            setCurrentPath(PathType.WORKER_MANAGE);
+          },
+        },
+        {
+          name: "근로자 출역관리",
+          icon: <People />,
+          path: PathType.WORKER_MANAGE,
+          onClick: () => {
+            setCurrentPath(PathType.WORKER_ENTERANCE_MANAGE);
+          },
+        },
+        {
+          name: "퇴직자 목록",
+          icon: <People />,
+          path: PathType.WORKER_MANAGE,
+          onClick: () => {
+            setCurrentPath(PathType.RETIREES_LIST);
+          },
+        },
+      ],
+    },
+    {
+      name: "근로자 안전점검",
+      icon: <ArrowForward />,
+      path: PathType.WORKER_SAFETY_CHECK,
+      onClick: () => {
+        setCurrentPath(PathType.WORKER_SAFETY_CHECK);
+      },
+    },
+    {
+      name: "안전보건지지침",
+      icon: <ArrowForward />,
+      path: PathType.OCCUPATIONAL_SAFETY_AND_HEALTH_ACT,
+      onClick: () => {
+        setCurrentPath(PathType.OCCUPATIONAL_SAFETY_AND_HEALTH_ACT);
+      },
+    },
+    {
+      name: "법령정보",
+      icon: <ArrowForward />,
+      path: PathType.LEGAL_INFORMATION,
+      onClick: () => {
+        setCurrentPath(PathType.LEGAL_INFORMATION);
+      },
+    },
+    {
+      name: "스케쥴러",
+      icon: <ArrowForward />,
+      path: PathType.SCHEDULER,
+      onClick: () => {
+        setCurrentPath(PathType.SCHEDULER);
+      },
+    },
     {
       name: "MEMO",
-      type: TabMenu.MEMO,
+      icon: <ArrowForward />,
+      path: PathType.MEMO,
+      onClick: () => {
+        setCurrentPath(PathType.MEMO);
+      },
     },
-
     {
-      name: "WORKER",
-      type: TabMenu.WORKER,
+      name: "TO DO LIST 관리",
+      icon: <ArrowForward />,
+      path: PathType.TO_DO_LIST_MANAGE,
+      onClick: () => {
+        setCurrentPath(PathType.TO_DO_LIST_MANAGE);
+      },
+    },
+    {
+      name: "비상연락망",
+      icon: <ArrowForward />,
+      path: PathType.EMERGENCY_CALL,
+      onClick: () => {
+        setCurrentPath(PathType.EMERGENCY_CALL);
+      },
+    },
+    {
+      name: "위험알림",
+      icon: <ArrowForward />,
+      path: PathType.RISK_NOTIFICATION,
+      onClick: () => {
+        setCurrentPath(PathType.RISK_NOTIFICATION);
+      },
     },
   ];
+  if (currentPath === undefined) {
+    return (
+      <div.wrap>
+        <Apps color="info" sx={{ width: 33, height: 33 }} />
+        <div style={{ minWidth: "110px" }}>패키지 센터</div>
+
+        <Divider orientation="vertical" variant="middle" flexItem />
+        <div.right>
+          {FIXED_LIST_DATA.slice(1).map((list, i) => (
+            <div.col
+              key={i}
+              onClick={() => {
+                setCurrentPath(list.path);
+              }}
+            >
+              <DescriptionOutlinedIcon />
+              {list.name}
+            </div.col>
+          ))}
+        </div.right>
+      </div.wrap>
+    );
+  }
 
   return (
-    <div>
-      <div>
-        {FIXED_TABS.map((tab, i) => (
-          <div onClick={() => setTab(tab.type)} key={i}>
-            {tab.name}
-          </div>
-        ))}
+    <div.sub>
+      <div className="side">
+        <NestedAccordion list={FIXED_LIST_DATA} />
       </div>
 
-      <div className="component">
-        {tab === TabMenu.TODO ? (
-          <div>todo</div>
-        ) : tab === TabMenu.MEMO ? (
-          <div>memo</div>
-        ) : (
-          <div>worker</div>
-        )}
-      </div>
-
-      <Apps color="info" sx={{ width: 66, height: 66 }} />
-    </div>
+      <div className="component">{renderCompontntByPath(currentPath)}</div>
+    </div.sub>
   );
 }
+
+const div = {
+  wrap: styled.div`
+    display: flex;
+    flex-direction: initial;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    margin: 50px;
+  `,
+  sub: styled.div`
+    display: flex;
+    justify-content: flex-start;
+    height: 100%;
+    .side {
+      width: 200px;
+      min-width: 200px;
+      border-right: 1px #e8e8e8 solid;
+    }
+    .component {
+      padding: 10px;
+    }
+  `,
+  right: styled.div`
+    display: flex;
+    flex-flow: row wrap;
+    width: 100%;
+  `,
+  col: styled.div`
+    flex-basis: 50%;
+    -webkit-box-flex: 0;
+    flex-grow: 0;
+    max-width: 50%;
+    width: 50%;
+    min-width: 200px;
+  `,
+};
---
+++ src/components/station/packageCenter/sub/retireesList.tsx
@@ -0,0 +1,223 @@
+import { useState } from "react";
+import dayjs, { Dayjs } from "dayjs";
+import {
+  Button,
+  InputLabel,
+  IconButton,
+  Paper,
+  InputBase,
+  Typography,
+} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { Search } from "@mui/icons-material";
+import {
+  DataGrid,
+  GridColDef,
+  GridRenderCellParams,
+  GridRowSelectionModel,
+  GridToolbarContainer,
+  GridToolbarExport,
+  koKR,
+} from "@mui/x-data-grid";
+import styled from "styled-components";
+
+const theme = createTheme(
+  {
+    palette: {
+      primary: { main: "#33518B" },
+    },
+  },
+  koKR
+);
+function CustomToolbar() {
+  return (
+    <GridToolbarContainer>
+      <GridToolbarExport />
+    </GridToolbarContainer>
+  );
+}
+export default function RetireesList() {
+  const [date, setDate] = useState<Dayjs | null>(dayjs());
+
+  const [rowSelectionWorker, setRowSelectionWorker] =
+    useState<GridRowSelectionModel>([]);
+
+  return (
+    <div>
+      <Typography fontSize={20} fontWeight={600}>
+        퇴직자 목록
+      </Typography>
+      <div.search>
+        <DatePicker
+          label="퇴직일"
+          value={date}
+          onChange={(newDate) => setDate(newDate)}
+          slotProps={{ textField: { size: "small" } }}
+        />
+        <Paper
+          component="form"
+          sx={{
+            p: "2px 4px",
+            display: "flex",
+            alignItems: "center",
+            width: 250,
+          }}
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="근로자 검색"
+            size="small"
+          />
+          <IconButton type="button" aria-label="search" size="small">
+            <Search />
+          </IconButton>
+        </Paper>
+        <Button variant="contained">퇴직자 추가</Button>
+      </div.search>
+
+      <ThemeProvider theme={theme}>
+        <DataGrid
+          checkboxSelection
+          rows={rows}
+          columns={columns}
+          pageSizeOptions={[25, 50, 100]}
+          paginationModel={{ page: 0, pageSize: 25 }}
+          onRowSelectionModelChange={(newRowSelectionWorker) => {
+            setRowSelectionWorker(newRowSelectionWorker);
+            console.log(newRowSelectionWorker);
+          }}
+          rowSelectionModel={rowSelectionWorker}
+          slots={{
+            toolbar: CustomToolbar,
+          }}
+        />
+      </ThemeProvider>
+    </div>
+  );
+}
+const columns: GridColDef[] = [
+  {
+    field: "id",
+    headerName: "NO",
+    headerAlign: "center",
+    align: "center",
+    width: 50,
+    renderCell: (params) => {
+      return params.api.getRowIndexRelativeToVisibleRows(params.id) + 1;
+    },
+  },
+  {
+    field: "workerName",
+    headerName: "이름",
+    headerAlign: "center",
+    align: "center",
+    width: 70,
+  },
+  {
+    field: "phone",
+    headerName: "연락처",
+    headerAlign: "center",
+    align: "center",
+    width: 150,
+  },
+  { field: "job", headerName: "직종", headerAlign: "center", align: "center" },
+  {
+    field: "workingDays",
+    headerName: "근무일수",
+    headerAlign: "center",
+    align: "center",
+  },
+  {
+    field: "teamName",
+    headerName: "소속팀",
+    headerAlign: "center",
+    align: "center",
+    width: 150,
+  },
+  {
+    field: "enteranceStartDate",
+    headerName: "입사일",
+    headerAlign: "center",
+    align: "center",
+  },
+  {
+    field: "retireedDate",
+    headerName: "퇴사일",
+    headerAlign: "center",
+    align: "center",
+    width: 200,
+  },
+  {
+    field: "workerId",
+    headerName: "변경",
+    headerAlign: "center",
+    sortable: false,
+    renderCell: (params) => {
+      const onClick = (e: any) => {
+        e.stopPropagation();
+        console.log(e);
+        console.log(params.api.getRowIndexRelativeToVisibleRows(params.id));
+      };
+
+      return (
+        <Button variant="contained" onClick={onClick}>
+          복귀
+        </Button>
+      );
+    },
+  },
+];
+
+const rows = [
+  {
+    id: "12312",
+    workerName: "김근로",
+    phone: "010-1234-1234",
+    job: "배관공",
+    workingDays: "20일",
+    teamName: "난방코일슬리브",
+    enteranceStartDate: "2023-03-15",
+    retireedDate: "2023-04-11 18:16",
+    workerId: "test1",
+  },
+  {
+    id: "345132",
+    workerName: "김기동",
+    phone: "010-1234-1234",
+    job: "안전관리자",
+    workingDays: "250일",
+    teamName: "난방코일슬리브",
+    enteranceStartDate: "2022-03-15",
+    retireedDate: "2023-04-11 18:16",
+    workerId: "test2",
+  },
+  {
+    id: "7889797",
+    workerName: "홍길동",
+    phone: "010-1234-1234",
+    job: "목공",
+    workingDays: "300일",
+    teamName: "난방코일슬리브",
+    enteranceStartDate: "2021-03-15",
+    retireedDate: "2023-04-11 18:16",
+    workerId: "test3",
+  },
+];
+
+const div = {
+  btween: styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+  `,
+
+  table: styled.div``,
+  search: styled.div`
+    display: flex;
+    flex-direction: row;
+    margin: 5% 0;
+    gap: 10px;
+  `,
+};
