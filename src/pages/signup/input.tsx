3f5d4f066d0c69a19d40d79b0360acb0a592142d
김진민 <jmkim0117@riskzero.kr>
2023-05-17 10:53:56 +0900

    회원가입 인풋 추가 중간커밋

--- src/pages/signup/input.tsx
+++ src/pages/signup/input.tsx
@@ -1,56 +1,52 @@
-import { useState } from "react";
-import { Typography, Checkbox, FormControlLabel } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Typography, TextField } from "@mui/material";
 import styled from "styled-components";
+import { passwordRule } from "@lib/inputRule";
 
 export default function SignUpInput() {
+
+  const [code, setCode] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [pwdCheck, setPwdCheck] = useState('');
+  const [telNo, setTelNo] = useState('');
+  const [name, setName] = useState('');
+
+  const [isValidPwd, setValidPwd] = useState(true);
+  const [isValidPwdCheck, setValidPwdCheck] = useState(true);
+
+  useEffect(() => {
+    if(password == '') return;
+
+    setValidPwd(passwordRule(password));
+    
+    setValidPwdCheck(false);
+  }, [password])
+  
+  useEffect(() => {
+    if(pwdCheck == '') return;
+
+    setValidPwdCheck(password === pwdCheck);
+  }, [pwdCheck])
+
   return (
     <div>
       <div>
-        <Typography fontSize={18}>서비스 이용약관</Typography>
+        <Typography fontSize={18}>회원 정보 입력</Typography>
       </div>
-
-      <div
-        style={{
-          height: 200,
-          padding: 10,
-          border: "1px #eee solid",
-          overflow: "auto",
-        }}
-      >
-        <FormControlLabel label="필수 약관 전체동의" control={<Checkbox />} />
-
-        <div style={{ paddingLeft: 30 }}>
-          <Typography>개인정보 수집 및 이용 목적에 대한 동의</Typography>
-          <Typography>(필수)</Typography>
-
-          <Typography>제 1 조 (목적)</Typography>
-          <Typography>
-            이 약관은 주식회사 리스크제로 (이하 “회사”라고 합니다)가 제공하는
-            “리스크제로 프로그램” (이하 "서비스"라 합니다)의 이용과 관련하여
-            "회원"과 "회사" 간에 필요한 사항을 규정함을 목적으로 합니다.
-          </Typography>
-          <Typography>상세보기 &gt;</Typography>
-          <FormControlLabel label="동의합니다." control={<Checkbox />} />
-        </div>
-
-        <div style={{ paddingLeft: 30 }}>
-          <Typography>개인정보 수집 및 이용 목적에 대한 동의</Typography>
-          <Typography>(필수)</Typography>
-
-          <Typography>제 1 조 (목적)</Typography>
-          <Typography>
-            이 약관은 주식회사 리스크제로 (이하 “회사”라고 합니다)가 제공하는
-            “리스크제로 프로그램” (이하 "서비스"라 합니다)의 이용과 관련하여
-            "회원"과 "회사" 간에 필요한 사항을 규정함을 목적으로 합니다.
-          </Typography>
-          <Typography>상세보기 &gt;</Typography>
-          <FormControlLabel label="동의합니다." control={<Checkbox />} />
-        </div>
-      </div>
+      <div.field><TextField label="계정 생성 코드" value={code} onChange={e => setCode(e.target.value)} variant="outlined" fullWidth /></div.field>
+      <div.field><TextField label="이메일" value={email} onChange={e => setEmail(e.target.value)} variant="outlined" fullWidth /></div.field>
+      <div.field><TextField label="비밀번호" value={password} onChange={e => setPassword(e.target.value)} error={!isValidPwd} variant="outlined" type="password" autoComplete="current-password" helperText="영문, 숫자, 특수기호(!,@,#,$,%) 모두 포함하여 8~15자리" fullWidth /></div.field>
+      <div.field><TextField label="비밀번호 확인" value={pwdCheck} onChange={e => setPwdCheck(e.target.value)} error={!isValidPwdCheck} variant="outlined" type="password" autoComplete="current-password" fullWidth /></div.field>
+      <div.field><TextField label="전화번호" value={telNo} onChange={e => setTelNo(e.target.value)} variant="outlined" fullWidth /></div.field>
+      <div.field><TextField label="이름" value={name} onChange={e => setName(e.target.value)} variant="outlined" helperText="실명을 입력해주세요" fullWidth /></div.field>
     </div>
   );
 }
 
 const div = {
   wrap: styled.div``,
+  field: styled.div`
+    margin-bottom: 10px;
+  `
 };
